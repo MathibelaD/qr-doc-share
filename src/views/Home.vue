fcba2a9f87e3cb4e8ae646be060079c461d7853f
MathibelaD <dineomathibela161@gmail.com>
2025-02-23 22:20:25 +0200

    fix: top margin

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <main>
     <!-- Hero Section -->
-    <section class="relative overflow-hidden bg-white">
+    <section class="relative overflow-hidden bg-white mt-20">
       <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-16 sm:py-24">
         <div class="lg:grid lg:grid-cols-12 lg:gap-8">
           <div 
