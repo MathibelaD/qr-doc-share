b5e2e0ad960636c444d24ace24b1df8ba533dd2e
MathibelaD <dineomathibela161@gmail.com>
2025-06-22 08:30:15 +0200

    added: serverless api functions

--- vite.config.ts
+++ vite.config.ts
@@ -12,7 +12,7 @@ export default defineConfig({
   ],
   server: {
     proxy: {
-      '/api': 'http://localhost:3001', // Proxy /api to your backend server
+      '/api': 'http://localhost:3000', // Proxy /api to your backend server
     },
   },
   resolve: {
